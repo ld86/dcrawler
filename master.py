50c68f220b1e713cea6bb643b36a68515ff14d54
Bogdan Melnik <ld@hackerdom.ru>
2015-03-18 22:13:54 +0500

    Take urls from config files

--- master.py
+++ master.py
@@ -1,6 +1,10 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from crawler import Master
+from json import load
+from sys import argv
 
-server = SimpleXMLRPCServer(('localhost', 8000), allow_none=True)
+options = load(open(argv[1]))
+server_address = (options['host'], options['port'])
+server = SimpleXMLRPCServer(server_address, allow_none=True)
 server.register_instance(Master())
 server.serve_forever()
--- slave.py
+++ slave.py
@@ -1,5 +1,9 @@
 import xmlrpclib
 from crawler import Slave
+from json import load
+from sys import argv
 
-master = xmlrpclib.ServerProxy('http://localhost:8000', allow_none=True)
+options = load(open(argv[1]))
+master_url = 'http://{0}:{1}'.format(options['host'], options['port'])
+master = xmlrpclib.ServerProxy(master_url, allow_none=True)
 Slave(master).start()
